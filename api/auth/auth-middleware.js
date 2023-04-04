2a57a037465cd1ead7f7c7d2ec90ac25af2b7cad
Joshua Kerr <kerrjoshua@gmail.com>
2023-04-04 15:44:11 +0900

    all tests passing

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -64,7 +64,7 @@ const checkUsernameExists = async (req, res, next) => {
   try {
     const [user] = await findBy({ username })
     if (!user) {
-      next({ status: 422, message: 'Invalid Credentials' })
+      next({ status: 401, message: 'Invalid Credentials' })
     } else {
       req.user = user
       next()
@@ -101,7 +101,7 @@ const validateRoleName = (req, res, next) => {
   } else if (req.body.role_name.trim() === 'admin') {
     next({ status: 422, message: 'Role name can not be admin' })
   } else if (req.body.role_name.trim().length > 32) {
-    next({ status: 422, message: 'Role name can not be longer than 32 characters' })
+    next({ status: 422, message: 'Role name can not be longer than 32 chars' })
   } else {
     req.role_name = req.body.role_name.trim();
     next()
